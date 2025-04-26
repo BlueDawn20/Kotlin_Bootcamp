2ebbd83770ee473dba858d7f4b110c9713bcf393
Selin Alp <87609765+BlueDawn20@users.noreply.github.com>
2025-04-26 19:24:03 +0300

    Add files via upload

---
+++ Odev1.kt
@@ -0,0 +1,143 @@
+fun main() {
+
+    // String türü
+
+    var sehir: String = "İstanbul"
+    println("Şehir: $sehir")
+
+    var ulke: String = "Türkiye"
+    println("Ülke: $ulke")
+
+    var telefon: String = "+90 532 123 4567"
+    println("Telefon: $telefon")
+
+    var postaKodu: String = "34000"
+    println("Posta Kodu: $postaKodu")
+
+    var email: String = "ornek@example.com"
+    println("Email: $email")
+
+    var meslek: String = "Öğretmen"
+    println("Meslek: $meslek")
+
+    var musteriAdi: String = "Ali Kaya"
+    println("Müşteri Adı: $musteriAdi")
+
+    var dogumGunu: String = "1990-12-05"
+    println("Doğum Günü: $dogumGunu")
+
+    var medeniDurum: String = "Evli"
+    println("Medeni Durum: $medeniDurum")
+
+    var urunYorum: String = "Ürün beklediğimden daha iyi çıktı"
+    println("Ürün Yorum: $urunYorum")
+
+    var arabaModeli: String = "BMW X5"
+    println("Araba Modeli: $arabaModeli")
+
+    var kitapAdi: String = "Kotlin Programlama"
+    println("Kitap Adı: $kitapAdi")
+
+    var yayinlamaTarihi: String = "2023-09-10"
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+
+    var urunAdi: String = "Kablosuz Kulaklık"
+    println("Ürün Adı: $urunAdi")
+
+    var marka: String = "Apple"
+    println("Marka: $marka")
+
+    var muzikAdi: String = "Shape of You"
+    println("Müzik Adı: $muzikAdi")
+
+    var videoSuresi: String = "3:45"
+    println("Video Süresi: $videoSuresi")
+
+    var resimAdi: String = "urun1.jpg"
+    println("Resim Adı: $resimAdi")
+
+    var dosyaFormati: String = ".png"
+    println("Dosya Formatı: $dosyaFormati")
+
+    var renk: String = "Mavi"
+    println("Renk: $renk")
+
+    var renkKodu: String = "#0000FF"
+    println("Renk Kodu: $renkKodu")
+
+    var telefonModeli: String = "iPhone 15"
+    println("Telefon Modeli: $telefonModeli")
+
+    var ulusalGun: String = "29 Ekim"
+    println("Ulusal Gün: $ulusalGun")
+
+    var tatilGunu: String = "1 Mayıs"
+    println("Tatil Günü: $tatilGunu")
+
+    var rezervasyonTarihi: String = "2025-08-12"
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+
+    var sokakAdi: String = "Atatürk Caddesi"
+    println("Sokak Adı: $sokakAdi")
+
+    var otobusHatti: String = "34AS"
+    println("Otobüs Hattı: $otobusHatti")
+
+    var takipKodu: String = "TR123456789"
+    println("Takip Kodu: $takipKodu")
+
+    var kuponKodu: String = "INDIRIM15"
+    println("Kupon Kodu: $kuponKodu")
+
+    var faturaAdresi: String = "Cumhuriyet Mah. No:20, İzmir"
+    println("Fatura Adresi: $faturaAdresi")
+
+    // Int türü
+
+    var stokMiktari: Int = 150
+    println("Stok Miktarı: $stokMiktari")
+
+    var siparisAdeti: Int = 3
+    println("Sipariş Adeti: $siparisAdeti")
+
+    var odaSayisi: Int = 2
+    println("Oda Sayısı: $odaSayisi")
+
+    var kalanDakika: Int = 8
+    println("Kalan Dakika: $kalanDakika")
+
+    var kuponSuresi: Int = 30
+    println("Kupon Süresi: $kuponSuresi")
+
+    // Double türü
+
+    var bakiye: Double = 975.50
+    println("Bakiye: $bakiye")
+
+    var maas: Double = 18500.0
+    println("Maaş: $maas")
+
+    var odeme: Double = 249.99
+    println("Ödeme: $odeme")
+
+    var indirimMiktari: Double = 50.0
+    println("İndirim Miktarı: $indirimMiktari")
+
+    var enlem: Double = 41.0082
+    println("Enlem: $enlem")
+
+    var boylam: Double = 28.9784
+    println("Boylam: $boylam")
+
+    var yemekFiyati: Double = 89.90
+    println("Yemek Fiyatı: $yemekFiyati")
+
+    var urunPuani: Double = 4.7
+    println("Ürün Puanı: $urunPuani")
+
+    var ekranBoyutu: Double = 6.5
+    println("Ekran Boyutu: $ekranBoyutu")
+
+    var agirlik: Double = 180.0
+    println("Ağırlık: $agirlik")
+}
